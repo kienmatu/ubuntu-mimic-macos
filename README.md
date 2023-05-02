d0b59b9c2ae064b43ce928a590e99b1b7f9fddeb
Kien Dinh <kienmatu1@gmail.com>
2023-05-02 10:35:16 +0700

    Feat: Add SF Pro options, update readme

--- README.md
+++ README.md
@@ -2,25 +2,44 @@
 Mimic macOS font on GNOME Ubuntu
 
 ## Introduction
-I want to bring the smooth font on macOS into Ubuntu GNOME.
+I want to bring the smooth font UX on macOS into Ubuntu GNOME.
 
 So I chose the font `Inter`, it is the font used on Figma and ElementaryOS, and it is open-source.
 
-You can also have a choice to change the font into `SF Pro` to have exactly what runs on mac.
+You also have a choice to change the font into `SF Pro` to have exactly what runs on mac.
 
-I also turned on the STEM darkening, that makes the font "bolder, thicker" like mac, but it doesn't work for HiDPI screen which has a big resolution like 4K.
+I also turned on the **STEM darkening**, that makes the font "bolder, thicker" like mac, but it doesn't work on HiDPI screen which has a big resolution like 4K.
 
-I also use the font alias to trigger `-apple-system`, `Arial` font on almost websites to have a smooth UX thorough all the machine.
+I also use the font alias to trigger `-apple-system`, `Arial` font on almost websites to have a smooth UX thorough all the apps.
 
 ## Instructions
 
-- Clone the repo
-- `cd ubuntu-mimic-macos`
-- run: `chmod +x tweak.sh`
-- run: `./tweak.sh`
+- Clone the repo:
+    ```bash
+    git clone https://github.com/kienmatu/ubuntu-mimic-macos.git
+    ```
+- Move to the folder:
+    ```bash
+    cd ubuntu-mimic-macos
+    ```
+- Make the file executable: 
+    ```bash
+    chmod +x ./tweak.sh
+    ```
+- run (without sudo)
+    ```bash
+    ./tweak.sh
+    ``` 
 - logout and login again.
 - tadaaaa
+### Options
+- `-sf`,`--sf-mode`: Using Sans Francisco instead of Inter
+- `-s`, `--size`: Change the font size, default font size is 11.
 
+Example:
+```bash
+./tweak.sh -s 12 -sf
+```
 
 ## Screenshots
 ### Before:
--- tweak.sh
+++ tweak.sh
@@ -1,27 +1,72 @@
 #!/bin/bash
 
-echo "Base font size is 11"
+# Set default values
+font_size='11'
+sf_mode=false
+alias_file=./fonts.conf
 
+# Parse arguments
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    -s|--size)
+    font_size="$2"
+    shift 
+    shift 
+    ;;
+    -sf|--sf-mode)
+    sf_mode=true
+    shift 
+    ;;
+    *)    
+    shift 
+    ;;
+esac
+done
+
+echo "Base font size is $font_size" 
+
+if [ "$sf_mode" = true ] ; then
+  echo "Using Sans Francisco Pro"
+  sudo cp -R fonts/SF_Pro/* /usr/share/fonts/truetype/SF\ Pro/
+  alias_file=./fonts_SF.conf
+else
+  echo "Using Inter font"
+fi
+
+echo "Installing presequite app..."
 sudo apt install fonts-inter gnome-tweaks font-manager
 
 # # Jetbrains mono
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/JetBrains/JetBrainsMono/master/install_manual.sh)"
 
+
+echo "Update GNOME configuration..."
+
 # update fonts
-gsettings set org.gnome.desktop.interface document-font-name 'Inter 11'
-gsettings set org.gnome.desktop.interface font-name 'Inter Display 11'
-gsettings set org.gnome.desktop.interface monospace-font-name 'Jetbrains Mono 12'
-gsettings set org.gnome.desktop.wm.preferences titlebar-font 'Inter Bold 12'
+if [ "$sf_mode" = true ] ; then
+  gsettings set org.gnome.desktop.interface document-font-name 'SF Pro Text 11'
+  gsettings set org.gnome.desktop.interface font-name 'SF Pro Display 11'
+  gsettings set org.gnome.desktop.interface monospace-font-name 'Jetbrains Mono 12'
+  gsettings set org.gnome.desktop.wm.preferences titlebar-font 'SF Pro Display Bold 12'
+else
+  gsettings set org.gnome.desktop.interface document-font-name 'Inter 11'
+  gsettings set org.gnome.desktop.interface font-name 'Inter Display 11'
+  gsettings set org.gnome.desktop.interface monospace-font-name 'Jetbrains Mono 12'
+  gsettings set org.gnome.desktop.wm.preferences titlebar-font 'Inter Bold 12'
+fi
+## ALIAS FILE
+config_dir=~/.config/fontconfig
 
-## ALIAS
-file=~/.config
-
-if [ ! -d ~/.config ]; then
-  mkdir ~/.config
+if [ ! -d file ]; then
+  mkdir -p file
 fi
 
-cp ./fonts.conf $file
+sudo cp "$alias_file" "$config_dir/fonts.conf"
 
+echo "Enabling STEM Darkening..."
 
 ## Enable STEM DARKENING
 ## Not useful for HiDPI monitor (4K)
@@ -29,8 +74,9 @@ if ! grep -qxF 'export FREETYPE_PROPERTIES="cff:no-stem-darkening=0"' ~/.profile
   echo 'export FREETYPE_PROPERTIES="cff:no-stem-darkening=0"' >> ~/.profile
 fi
 
-source ~/.profile
 ## >> is append, > is overwrite
 
+echo "clearing cache..."
+fc-cache -fv
 
 echo "Successfully, please logout and login again!"
